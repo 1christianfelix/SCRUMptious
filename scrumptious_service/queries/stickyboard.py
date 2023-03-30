718c696b9ce002a0926367f7ac6f5f80da96f458
Kurt Loban <kloban01@gmail.com>
2023-03-30 15:24:06 -0700

    Worked on FastAPI endpoints for User: Post, Get, Put, Delete

--- scrumptious_service/queries/stickyboard.py
+++ scrumptious_service/queries/stickyboard.py
@@ -1,7 +1,7 @@
 from typing import Optional
 from pydantic import BaseModel
 from datetime import datetime
-
+from queries.pool import db
 
 collection = db["StickyBoard"]
 
---
+++ scrumptious_service/routers/user.py
@@ -0,0 +1,58 @@
+from fastapi import APIRouter, Depends, HTTPException
+from queries.user import User, UserQueries
+from queries.pool import db
+
+router = APIRouter()
+
+user_queries = UserQueries()
+
+#signup
+@router.post("/user")
+def create_user(user: User):
+    created_user = user_queries.create_user(user)
+    if created_user:
+        return created_user
+    else:
+        raise HTTPException(status_code=500, detail="Could not create user")
+
+
+#get fools
+@router.get("/user/{user_id}")
+def get_user_by_id(user_id: str):
+    user = user_queries.get_user_by_id(user_id)
+    if user:
+        return user
+    else:
+        raise HTTPException(status_code=404, detail="User not found")
+
+#edit fools
+@router.put("/user/{user_id}")
+def update_user(user_id: str, user: User):
+    updated_user = user_queries.update_user(user_id, user)
+    if updated_user:
+        return updated_user
+    else:
+        raise HTTPException(status_code=404, detail="User not found")
+
+#delete fools
+# @router.delete("/user/{user_id}")
+# def delete_user(
+#     user_id: str, user_queries: UserQueries = Depends()
+# ):
+#     return user_queries.delete_user(user_id)
+
+
+@router.delete("/user/{user_id}")
+def delete_user(user_id: str):
+    deleted_user = user_queries.delete_user(user_id)
+    if deleted_user:
+        return deleted_user
+    else:
+        raise HTTPException(status_code=404, detail="User not found")
+
+
+
+
+# get user + Sticky
+# get user + stickyboard
+# get user + sticky + StickyBoard
