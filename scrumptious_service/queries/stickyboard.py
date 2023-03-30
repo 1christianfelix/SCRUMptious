6b2b1eb1173dd58ae51cd6fb0f4176309cd1dfaf
John Chaohui Liu <zodram@gmail.com>
2023-03-30 22:20:36 +0000

    #1, #7, #9, #4, #2, #26 John Liu - Complete the POST, 2 GET and DELETE function for stickboard; the POST and GET function for sticky

--- scrumptious_service/queries/stickyboard.py
+++ scrumptious_service/queries/stickyboard.py
@@ -1,6 +1,8 @@
 from typing import Optional
 from pydantic import BaseModel
 from datetime import datetime
+from queries.pool import db
+from bson import ObjectId
 
 
 collection = db["StickyBoard"]
@@ -11,20 +13,13 @@ class StickyBoard(BaseModel):
     priority: int
     startDate: datetime
     deadline: datetime
-    user: Optional[str]
+    user: list[str] 
 
-
-class StickyBoardUpdate(BaseModel):
-    boardName: Optional[str]
-    priority: Optional[int]
-    startDate: Optional[datetime]
-    deadline: Optional[datetime]
-    user: Optional[str]
 
 
 class StickyBoardQueries:
     def get_stickyboard_by_id(self, stickyboard_id):
-        result = collection.find_one({"_id": stickyboard_id})
+        result = collection.find_one({"_id": ObjectId(stickyboard_id)})
         if result:
             result["id"] = str(result["_id"])
             del result["_id"]
@@ -44,7 +39,7 @@ class StickyBoardQueries:
             return results
 
     def get_stickyboard_stickies(self, stickyboard_id):
-        results = list(db["Sticky"].find({"stickyboard": stickyboard_id}))
+        results = list(db["Sticky"].find({"stickyBoard": stickyboard_id}))
         for result in results:
             result["id"] = str(result["_id"])
             del result["_id"]
@@ -55,6 +50,6 @@ class StickyBoardQueries:
         pass
 
     def delete_stickyboard(self, stickyboard_id):
-        result = collection.delete_one({"_id": stickyboard_id})
+        result = collection.delete_one({"_id": ObjectId(stickyboard_id)})
         if result:
             return True
