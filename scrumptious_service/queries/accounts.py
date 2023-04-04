431aa68412eac2551f7557fe4fe931b0bce5657b
John Liu <zodram@gmail.com>
2023-04-03 19:01:56 -0700

    complete the get all accounts (GET) and delete an account (DELETE) functions

--- scrumptious_service/queries/accounts.py
+++ scrumptious_service/queries/accounts.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 from pymongo.errors import DuplicateKeyError
 from queries.pool import client
+from bson import ObjectId
 
 
 
@@ -46,3 +47,20 @@ class AccountQueries:
             raise DuplicateAccountError()
         props["id"] = str(props["_id"])
         return AccountOutWithPassword(**props)
+
+    def get_all_accounts(self):
+        results = list(collection.find())
+        for result in results:
+            result["id"] = str(result["_id"])
+            del result["_id"]
+            del result["password"]
+            del result["hashed_password"]
+        if results:
+            return results
+
+    def delete_account(self, account_id):
+        result = collection.delete_one({"_id": ObjectId(account_id)})
+        if result.deleted_count:
+            return "The account is deleted."
+        else:
+            return "The account does not exist."
