6ac0ee435923ec43ce94aedd821a20afdacf3d1e
Christian Felix <1christianfelix@gmail.com>
2023-04-04 00:52:09 -0700

    Created UI/UX and form layout for the signup page.

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -2,41 +2,46 @@ import "./App.css";
 import Sidebar from "./components/Sidebar";
 import Dashboard from "./pages/Dashboard";
 import StickyNote from "./components/StickyNote";
-import React, {useEffect, useState} from 'react';
-import { Routes, Route } from 'react-router-dom';
-import StickyBoardCreateForm from './components/StickyBoardCreateForm';
-
+import React, { useEffect, useState } from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import StickyBoardCreateForm from "./components/StickyBoardCreateForm";
+import Signup from "./pages/Signup";
 
 function App() {
-
-  const[users, setUsers] = useState([])
+  const [users, setUsers] = useState([]);
   const getUserData = async () => {
-  const userUrl = 'http://localhost:8000/user/';
-  const userResponse = await fetch(userUrl);
-  if (userResponse.ok) {
-    const data = await userResponse.json();
-    console.log(data)
-    setUsers(data.users)
-  }
-}
-useEffect(() => {getUserData()}, []);
-
+    const userUrl = "http://localhost:8000/user/";
+    const userResponse = await fetch(userUrl);
+    if (userResponse.ok) {
+      const data = await userResponse.json();
+      console.log(data);
+      setUsers(data.users);
+    }
+  };
+  useEffect(() => {
+    getUserData();
+  }, []);
 
   return (
-    <div className="flex font-Sudo_Var">
-      {/* The sidebar takes up x amount of the space */}
-      <Sidebar>
-        <Routes>
-          <Route path="stickyboard">
-            <Route path="new" element={<StickyBoardCreateForm users={users} />} />
-          </Route>
-        </Routes>
-      </Sidebar>
-      {/* The dashboard represents everything else to the right of the sidebar */}
-      <Dashboard>
-
-      </Dashboard>
-    </div>
+    <BrowserRouter>
+      <Signup></Signup>
+      {false && (
+        <div className="flex font-Sudo_Var">
+          {/* The sidebar takes up x amount of the space */}
+          <Sidebar></Sidebar>
+          {/* The dashboard represents everything else to the right of the sidebar */}
+          <Dashboard></Dashboard>
+          <Routes>
+            <Route path="stickyboard">
+              <Route
+                path="new"
+                element={<StickyBoardCreateForm users={users} />}
+              />
+            </Route>
+          </Routes>
+        </div>
+      )}
+    </BrowserRouter>
   );
 }
 
