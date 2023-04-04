6198b14083bde74b5d82232d0c094c4b8352834d
1christianfelix1 <1christianfelix@gmail.com>
2023-04-04 22:04:45 +0000

    #34 #35 Christian F - Created UI/UX for Sign Up and Sign In pages

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -2,40 +2,54 @@ import "./App.css";
 import Sidebar from "./components/Sidebar";
 import Dashboard from "./pages/Dashboard";
 import StickyNote from "./components/StickyNote";
-import React, {useEffect, useState} from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import StickyBoardCreateForm from './components/StickyBoardCreateForm';
-
+import React, { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import StickyBoardCreateForm from "./components/StickyBoardCreateForm";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const getUserData = async () => {
+    const userUrl = "http://localhost:8000/user/";
+    const userResponse = await fetch(userUrl);
+    if (userResponse.ok) {
+      const data = await userResponse.json();
+      console.log(data);
+      setUsers(data.users);
+    }
+  };
+  useEffect(() => {
+    getUserData();
+  }, []);
 
-  const[accounts, setAccounts] = useState([])
+  const [accounts, setAccounts] = useState([]);
   const getAccountsData = async () => {
-  const accountUrl = 'http://localhost:8000/accounts';
-  const accountResponse = await fetch(accountUrl);
-  if (accountResponse.ok) {
-    const data = await accountResponse.json();
-    setAccounts(data)
-  }
-}
-useEffect(() => {getAccountsData()}, []);
-
+    const accountUrl = "http://localhost:8000/accounts";
+    const accountResponse = await fetch(accountUrl);
+    if (accountResponse.ok) {
+      const data = await accountResponse.json();
+      setAccounts(data);
+    }
+  };
+  useEffect(() => {
+    getAccountsData();
+  }, []);
 
   return (
     <BrowserRouter>
       <div className="flex font-Sudo_Var">
         {/* The sidebar takes up x amount of the space */}
-        <Sidebar>
-
-        </Sidebar>
+        <Sidebar></Sidebar>
         {/* The dashboard represents everything else to the right of the sidebar */}
-        <Dashboard>
-
-        </Dashboard>
+        <Dashboard></Dashboard>
 
         <Routes>
           <Route path="stickyboard">
-            <Route path="new" element={<StickyBoardCreateForm accounts={accounts} />} />
+            <Route
+              path="new"
+              element={<StickyBoardCreateForm accounts={accounts} />}
+            />
           </Route>
         </Routes>
       </div>
