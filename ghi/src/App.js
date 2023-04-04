f9c6581b08bfc6128f37ce7dc7b3dbb9d32c0a8c
Christian Felix <1christianfelix@gmail.com>
2023-04-04 13:23:49 -0700

    Cleaned app.js merge

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -5,6 +5,8 @@ import StickyNote from "./components/StickyNote";
 import React, { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import StickyBoardCreateForm from "./components/StickyBoardCreateForm";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
 
 function App() {
   const [users, setUsers] = useState([]);
