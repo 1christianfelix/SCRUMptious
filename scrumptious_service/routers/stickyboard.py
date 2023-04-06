ec2ff5452f3566b4117f2157f352f57d92679cec
Christian Felix <1christianfelix@gmail.com>
2023-04-06 16:06:26 -0700

    protecting endpoints

--- scrumptious_service/routers/stickyboard.py
+++ scrumptious_service/routers/stickyboard.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends
+from authenticator import authenticator
 from queries.stickyboard import StickyBoard, StickyBoardQueries
 
 
@@ -7,23 +8,23 @@ router = APIRouter()
 
 @router.post("/stickyboard")
 def create_stickyboard(
-    sticky: StickyBoard, queries: StickyBoardQueries = Depends()
+    sticky: StickyBoard, queries: StickyBoardQueries = Depends(), account_data: dict = Depends(authenticator.get_current_account_data)
 ):
     return queries.create_stickyboard(sticky)
 
 
 @router.get("/stickyboard")
-def get_stickyboards(queries: StickyBoardQueries = Depends()):
+def get_stickyboards(queries: StickyBoardQueries = Depends(), account_data: dict = Depends(authenticator.get_current_account_data)):
     return queries.get_stickyboards()
 
 @router.get("/stickyboard/{stickyboard_id}")
-def get_stickyboard_by_id(stickyboard_id: str, queries: StickyBoardQueries = Depends()):
+def get_stickyboard_by_id(stickyboard_id: str, queries: StickyBoardQueries = Depends(), account_data: dict = Depends(authenticator.get_current_account_data)):
     return queries.get_stickyboard_by_id(stickyboard_id)
 
 
 @router.get("/stickyboard/{stickyboard_id}/sticky")
 def get_stickyboard_stickies(
-    stickyboard_id: str, queries: StickyBoardQueries = Depends()
+    stickyboard_id: str, queries: StickyBoardQueries = Depends(), account_data: dict = Depends(authenticator.get_current_account_data)
 ):
     return queries.get_stickyboard_stickies(stickyboard_id)
 
@@ -32,7 +33,7 @@ def get_stickyboard_stickies(
 def update_stickyboard(
     stickyboard_id: str,
     stickyboard: StickyBoard,
-    queries: StickyBoardQueries = Depends(),
+    queries: StickyBoardQueries = Depends(), account_data: dict = Depends(authenticator.get_current_account_data)
 ):
     return queries.update_sticky(stickyboard_id, stickyboard)
 
@@ -40,6 +41,6 @@ def update_stickyboard(
 @router.delete("/stickyboard/{stickyboard_id}")
 def delete_stickyboard(
     stickyboard_id: str,
-    queries: StickyBoardQueries = Depends()
+    queries: StickyBoardQueries = Depends(), account_data: dict = Depends(authenticator.get_current_account_data)
 ):
     return queries.delete_stickyboard(stickyboard_id)
