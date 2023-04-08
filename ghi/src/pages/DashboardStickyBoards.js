6770190bf582cb5e2a3c01fcbfa9240a9c6b3263
Christian Felix <1christianfelix@gmail.com>
2023-04-07 18:00:40 -0700

    Added comments

--- ghi/src/pages/DashboardStickyBoards.js
+++ ghi/src/pages/DashboardStickyBoards.js
@@ -6,9 +6,9 @@ import filter_icon_white from "../images/icons/filter_icon_white.svg";
 
 const DashboardStickyBoards = () => {
   return (
-    <div className="p-20 flex flex-col gap-10 bg-blue-300 h-screen">
-      <div className="flex items-center justify-center gap-10">
-        <div className="flex items-center justify-between bg-white rounded-[100px] w-[49rem] h-[4.75rem] px-10 text-2xl ">
+    <div className="px-20 pt-20 flex flex-col gap-10 h-screen">
+      <div className="flex gap-10">
+        <div className="flex items-center justify-between bg-white rounded-[100px] w-[25rem] h-[4.75rem] px-10 text-2xl ">
           <input
             type="text"
             placeholder="Search Sticky Boards"
@@ -21,9 +21,7 @@ const DashboardStickyBoards = () => {
             Start a Sticky Board
           </span>
         </button>
-      </div>
-      <div className="flex-grow bg-slate-400">
-        <div className="justify-self-end self-end flex gap-5">
+        <div className=" flex gap-5 justify-end">
           <div className="flex gap-2 items-center">
             <img src={filter_icon_white} alt="" className="w-[1rem] h-[1rem]" />
 
@@ -31,7 +29,10 @@ const DashboardStickyBoards = () => {
               Filter Priority:
             </label>
           </div>
-          <div id="priority" className="text-dark_mode_text_white flex gap-2">
+          <div
+            id="priority"
+            className="text-dark_mode_text_white flex self-center gap-2"
+          >
             <input type="radio" id="high" name="priority" value="high" />
             <label for="high">High</label>
             <input type="radio" id="medium" name="priority" value="medium" />
@@ -40,7 +41,22 @@ const DashboardStickyBoards = () => {
             <label for="low">Low</label>
           </div>
         </div>
-        <div className="place-items-center grid grid-cols-4 gap-y-10">
+      </div>
+      <div className="flex-grow overflow-auto scrollbar-card">
+        <div className="place-items-center grid grid-cols-4 gap-y-10 last:mb-10">
+          {/* Everything below is for example purposes. Map function would go below here  */}
+          <StickyBoardCard
+            priority="High"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pulvinar elementum integer enim neque volutpat ac. Tincidunt nunc pulvinar sapien et. Ultricies mi quis hendrerit dolor magna eget. Etiam dignissim diam quis enim. Viverra ipsum nunc aliquet bibendum enim"
+          ></StickyBoardCard>
+          <StickyBoardCard
+            priority="High"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pulvinar elementum integer enim neque volutpat ac. Tincidunt nunc pulvinar sapien et. Ultricies mi quis hendrerit dolor magna eget. Etiam dignissim diam quis enim. Viverra ipsum nunc aliquet bibendum enim"
+          ></StickyBoardCard>
+          <StickyBoardCard
+            priority="High"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pulvinar elementum integer enim neque volutpat ac. Tincidunt nunc pulvinar sapien et. Ultricies mi quis hendrerit dolor magna eget. Etiam dignissim diam quis enim. Viverra ipsum nunc aliquet bibendum enim"
+          ></StickyBoardCard>
           <StickyBoardCard
             priority="High"
             content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pulvinar elementum integer enim neque volutpat ac. Tincidunt nunc pulvinar sapien et. Ultricies mi quis hendrerit dolor magna eget. Etiam dignissim diam quis enim. Viverra ipsum nunc aliquet bibendum enim"
@@ -73,6 +89,51 @@ const DashboardStickyBoards = () => {
             priority="Medium"
             content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut"
           ></StickyBoardCard>
+          <StickyBoardCard
+            priority="High"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pulvinar elementum integer enim neque volutpat ac. Tincidunt nunc pulvinar sapien et. Ultricies mi quis hendrerit dolor magna eget. Etiam dignissim diam quis enim. Viverra ipsum nunc aliquet bibendum enim"
+          ></StickyBoardCard>
+          <StickyBoardCard
+            priority="High"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pulvinar elementum integer enim neque volutpat ac. Tincidunt nunc pulvinar sapien et. Ultricies mi quis hendrerit dolor magna eget. Etiam dignissim diam quis enim. Viverra ipsum nunc aliquet bibendum enim"
+          ></StickyBoardCard>
+          <StickyBoardCard
+            priority="High"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pulvinar elementum integer enim neque volutpat ac. Tincidunt nunc pulvinar sapien et. Ultricies mi quis hendrerit dolor magna eget. Etiam dignissim diam quis enim. Viverra ipsum nunc aliquet bibendum enim"
+          ></StickyBoardCard>
+          <StickyBoardCard
+            priority="High"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pulvinar elementum integer enim neque volutpat ac. Tincidunt nunc pulvinar sapien et. Ultricies mi quis hendrerit dolor magna eget. Etiam dignissim diam quis enim. Viverra ipsum nunc aliquet bibendum enim"
+          ></StickyBoardCard>
+          <StickyBoardCard
+            priority="Medium"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut"
+          ></StickyBoardCard>
+          <StickyBoardCard
+            priority="Low"
+            content="Lorem ipsum dolor sit amet,"
+          ></StickyBoardCard>
+          <StickyBoardCard
+            priority="High"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pulvinar elementum integer enim neque volutpat ac. Tincidunt nunc pulvinar sapien et. Ultricies mi quis hendrerit dolor magna eget. Etiam dignissim diam quis enim. Viverra ipsum nunc aliquet bibendum enim"
+          ></StickyBoardCard>
+          <StickyBoardCard
+            priority="Medium"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut"
+          ></StickyBoardCard>
+          <StickyBoardCard
+            priority="Low"
+            content="Lorem ipsum dolor sit amet,"
+          ></StickyBoardCard>
+          <StickyBoardCard
+            priority="High"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pulvinar elementum integer enim neque volutpat ac. Tincidunt nunc pulvinar sapien et. Ultricies mi quis hendrerit dolor magna eget. Etiam dignissim diam quis enim. Viverra ipsum nunc aliquet bibendum enim"
+          ></StickyBoardCard>
+          <StickyBoardCard
+            priority="Medium"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut"
+          ></StickyBoardCard>
+          {/* Everything above is for example purposes. Map function would go above here */}
         </div>
       </div>
     </div>
