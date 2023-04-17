c98c73ec62cf6122c4283465ebf875682577cdf3
Christian Felix <1christianfelix@gmail.com>
2023-04-17 01:15:51 -0700

    Fixed allignment issues with modal

--- ghi/src/pages/DashboardStickyBoards.js
+++ ghi/src/pages/DashboardStickyBoards.js
@@ -40,74 +40,80 @@ const DashboardStickyBoards = () => {
   };
 
   return (
-    <div className="px-20 pt-20 flex flex-col gap-10 h-screen">
+    <div className=" h-screen overflow-hidden">
       <StickyBoardInputForm
         open={modalStatus}
         close={handleCloseModal}
         type="Update"
       />
-      <div className="flex gap-10 items-center">
-        <div className="flex items-center justify-between bg-white rounded-[100px] w-[25rem] h-[4.75rem] px-10 text-2xl">
-          <input
-            type="text"
-            placeholder="Search Sticky Boards"
-            className="focus:outline-none w-[100%]"
-          />
-          <img src={Search_light} alt="" className="h-[2rem] w-[2rem]" />
-        </div>
-        <button
-          className="text-white border-solid border-button rounded-[19px] w-[16rem] h-[4rem] button-hover-white-outline"
-          onClick={handleOpenModal}
-        >
-          Start a Sticky Board
-        </button>
-        <div className=" flex gap-5 justify-end">
-          <div className="flex gap-2 items-center">
-            <img src={filter_icon_white} alt="" className="w-[1rem] h-[1rem]" />
-
-            <label for="priority" className="text-dark_mode_text_white">
-              Filter Priority:
-            </label>
+      <div className="px-20 pt-20 flex flex-col gap-10 overflow-hidden h-[100%]">
+        <div className="flex gap-10 items-center">
+          <div className="flex items-center justify-between bg-white rounded-[100px] w-[25rem] h-[4.75rem] px-10 text-2xl">
+            <input
+              type="text"
+              placeholder="Search Sticky Boards"
+              className="focus:outline-none w-[100%]"
+            />
+            <img src={Search_light} alt="" className="h-[2rem] w-[2rem]" />
           </div>
-          <div
-            id="priority"
-            className="text-dark_mode_text_white flex self-center gap-2"
+          <button
+            className="text-white border-solid border-button rounded-[19px] w-[16rem] h-[4rem] button-hover-white-outline"
+            onClick={handleOpenModal}
           >
-            <input type="radio" id="high" name="priority" value="high" />
-            <label for="high">High</label>
-            <input type="radio" id="medium" name="priority" value="medium" />
-            <label for="medium">Medium</label>
-            <input type="radio" id="low" name="priority" value="low" />
-            <label for="low">Low</label>
+            Start a Sticky Board
+          </button>
+          <div className=" flex gap-5 justify-end">
+            <div className="flex gap-2 items-center">
+              <img
+                src={filter_icon_white}
+                alt=""
+                className="w-[1rem] h-[1rem]"
+              />
+
+              <label for="priority" className="text-dark_mode_text_white">
+                Filter Priority:
+              </label>
+            </div>
+            <div
+              id="priority"
+              className="text-dark_mode_text_white flex self-center gap-2"
+            >
+              <input type="radio" id="high" name="priority" value="high" />
+              <label for="high">High</label>
+              <input type="radio" id="medium" name="priority" value="medium" />
+              <label for="medium">Medium</label>
+              <input type="radio" id="low" name="priority" value="low" />
+              <label for="low">Low</label>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="flex-grow overflow-auto scrollbar-card hover:scrollbar-thumb-slate-300 scrollbar-thumb-white scrollbar-w-2">
-        <div className="place-items-center grid grid-cols-4 gap-y-10 last:mb-10">
-          {boards.map((board, index) => {
-            return (
-              <div className="relative">
-                <StickyBoardCard
-                  priority={board.priority.toString()}
-                  content={board.content}
-                  members={board.members}
-                  name={"Board Name prop"}
-                />
-                <div className="BUTTONS flex flex-col justify-between py-3">
-                  <img
-                    src={garbage}
-                    className="expand-button absolute bottom-9 left-5"
+        <div className="flex-grow overflow-auto scrollbar-card hover:scrollbar-thumb-slate-300 scrollbar-thumb-white scrollbar-w-2">
+          <div className="place-items-center grid grid-cols-4 gap-y-10 last:mb-10">
+            {boards.map((board, index) => {
+              return (
+                <div className="relative">
+                  <StickyBoardCard
+                    priority={board.priority.toString()}
+                    content={board.content}
+                    members={board.members}
+                    name={"Board Name prop"}
                   />
-                  <button
-                    className="button-hover-white-filled px-[.7rem] py-[.1rem] bg-white rounded-[19px] absolute bottom-9 right-5"
-                    onClick={handleOpenModal}
-                  >
-                    <span>Edit Board</span>
-                  </button>
+                  <div className="BUTTONS flex flex-col justify-between py-3">
+                    <img
+                      src={garbage}
+                      className="expand-button absolute bottom-9 left-5"
+                    />
+                    <button
+                      className="button-hover-white-filled px-[.7rem] py-[.1rem] bg-white rounded-[19px] absolute bottom-9 right-5"
+                      onClick={handleOpenModal}
+                    >
+                      <span>Edit Board</span>
+                    </button>
+                  </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
