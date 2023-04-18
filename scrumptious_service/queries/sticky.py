adec46028b6d518ac8bfc110bfb14fe12b8ff770
Brandon <BMoore8791@gmail.com>
2023-04-18 11:47:38 -0400

    delete, update sticky back/front

--- scrumptious_service/queries/sticky.py
+++ scrumptious_service/queries/sticky.py
@@ -58,10 +58,7 @@ class StickyQueries:
 
     def update_sticky(self, sticky_id, sticky):
         sticky_id = ObjectId(sticky_id)
-
         original_sticky = collection.find_one({"_id": ObjectId(sticky_id)})
-
-
         result = collection.update_one(
             {"_id": sticky_id},
             {"$set": sticky.dict(exclude_unset=True)}
@@ -69,23 +66,11 @@ class StickyQueries:
         if result.modified_count:
             updated_sticky = collection.find_one({"_id": ObjectId(sticky_id)})
             if updated_sticky["category"] != original_sticky["category"]:
-                print("original sticky:", original_sticky)
-                print("Update sticky:", updated_sticky)
-                # props = sticky.dict()
-                # props['stickyboard'] = stickyboard_id
-                # db["Sticky"].insert_one(props)
                 stickyboard = db["StickyBoard"].find_one({"_id": ObjectId(updated_sticky["stickyboard"])})
-                # category_list = stickyboard[props["category"]]
-                # category_list.append(str(props["_id"]))
-                print("original_sticky category:", original_sticky["category"])
-                print("sticky id", updated_sticky["_id"])
                 category_list_for_removal = stickyboard[original_sticky["category"]]
-                print("category_list_for_removal: ", category_list_for_removal)
                 category_list_for_removal.remove(str(updated_sticky["_id"]))
-                print("category_list_for_removal:", category_list_for_removal)
                 category_list_for_appending = stickyboard[updated_sticky["category"]]
                 category_list_for_appending.append(str(updated_sticky["_id"]))
-                print("category_list_for_appending-->", category_list_for_appending)
                 db["StickyBoard"].update_one(
                     {"_id": ObjectId(updated_sticky["stickyboard"])},
                     {"$set": {updated_sticky["category"]: category_list_for_appending,
