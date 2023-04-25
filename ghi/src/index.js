237f609afb11aa9b1f6a630dc335d812a969e730
Christian Felix <1christianfelix@gmail.com>
2023-04-25 15:43:05 -0700

    Uncommented index domain and basename

--- ghi/src/index.js
+++ ghi/src/index.js
@@ -11,7 +11,7 @@ const domain = /https:\/\/[^/]+/;
 const basename = process.env.PUBLIC_URL.replace(domain, "");
 
 root.render(
-  <BrowserRouter basename={"/"}>
+  <BrowserRouter basename={basename}>
     <AuthProvider
       tokenUrl={`${process.env.REACT_APP_USER_SERVICE_API_HOST}/token`}
     >
