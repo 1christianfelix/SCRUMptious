bf38ac237b264e2e604f2318d7a7f54508ae4feb
Kurt Loban <kloban01@gmail.com>
2023-04-25 15:03:24 -0700

    Reviewing for John, need to commit

---
+++ scrumptious_service/queries/send_email.py
@@ -0,0 +1,20 @@
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
+
+
+def send_email(to_emails, subject, content):
+    message = Mail(
+        from_email='ScrumptiousTeam@hotmail.com',
+        to_emails=to_emails,
+        subject=subject,
+        html_content=content
+        )
+    try:
+        sg = SendGridAPIClient(SENDGRID_API_KEY)
+        response = sg.send(message)
+        return response.status_code == 202
+    except Exception:
+        return False
