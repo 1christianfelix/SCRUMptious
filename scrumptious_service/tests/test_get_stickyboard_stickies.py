ab524b1aa087c0d5e5562892e6daa1b7f667a083
Christian Felix <1christianfelix@gmail.com>
2023-04-26 12:12:09 -0700

    Unit Test for test_get_stickyboard_stickies is successful

--- scrumptious_service/tests/test_get_stickyboard_stickies.py
+++ scrumptious_service/tests/test_get_stickyboard_stickies.py
@@ -5,23 +5,24 @@ from authenticator import authenticator
 
 client = TestClient(app)
 
-stickyboard_id = "6447770a53ecaa2913b42f9b"
-sticky_note = {
-    "subject": "f",
-    "content": "f",
-    "priority": 2,
-    "category": "done",
-    "start_date": "2023-04-11T07:00:00",
-    "deadline": "2023-04-10T07:00:00",
-    "account": [],
-    "append": False,
-    "stickyboard": stickyboard_id,
-    "id": "6447771e53ecaa2913b42f9c"
-}
+
+
 
 
 class GetStickiesFromStickyBoardQueries:
-     def get_stickies_data(self):
+     def get_stickyboard_stickies(self,stickyboard_id):
+          sticky_note = {
+              "subject": "f",
+              "content": "f",
+              "priority": 2,
+              "category": "done",
+              "start_date": "2023-04-11T07:00:00",
+              "deadline": "2023-04-10T07:00:00",
+              "account": [],
+              "append": False,
+              "stickyboard": stickyboard_id,
+              "id": "6447771e53ecaa2913b42f9c"
+           }
           stickyboard_categories ={
                       "backlog": [],
                       "todo": [],
@@ -45,6 +46,7 @@ def test_get_stickyboard_stickies():
     app.dependency_overrides[
         authenticator.get_current_account_data
     ] = account_override
+    stickyboard_id = "6447770a53ecaa2913b42f9b"
     response = client.get(f"/stickyboard/{stickyboard_id}/sticky")
     app.dependency_overrides = {}
     expected = {
